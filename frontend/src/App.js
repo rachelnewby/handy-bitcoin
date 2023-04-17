c286c337a719f3b54951a5b1e4869ce318d36ebd
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-04-17 17:54:32 +0100

    added fetch request that runs on 3 second intervals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HandTracker from "./components/handTracker/handTracker";
 import UserTutorial from "./components/userTutorial/userTutorial";
+import BitcoinLivePrice from "./components/bitcoinLivePrice/bitcoinLivePrice";
 
 function App() {
   return (
     <div className="App">
       <UserTutorial />
       <HandTracker />
+      <BitcoinLivePrice />
     </div>
   );
 }
--- frontend/src/components/bitcoinLivePrice/bitcoinLivePrice.js
+++ frontend/src/components/bitcoinLivePrice/bitcoinLivePrice.js
@@ -4,9 +4,18 @@ import BitcoinLogo from "../images/Bitcoin.png";
 const BitcoinLivePrice = () => {
   const [currentPrice, setCurrentPrice] = useState();
 
+  setInterval(() => {
+    fetch("http://localhost:4000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentPrice(data.currentPrice);
+      });
+  }, 3000);
+
   return (
     <div id="bitcoin-price-container">
       <img id="bitcoin-logo" src={BitcoinLogo} />
+      <h2>{currentPrice}</h2>
     </div>
   );
 };
